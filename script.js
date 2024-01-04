9134b77aab8eb48bc62137780736e7aa25277877
kendpr <real.kendpr@gmail.com>
2024-01-04 15:05:03 +0800

    I forgot to commit the JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const submitBtn = document.getElementsByClassName("submit");
+const emailInput = document.getElementById("email");
+
+const defaultCard = document.getElementsByClassName("card");
+const successCard = document.getElementsByClassName("success-card");
+const submittedEmail = document.getElementsByClassName("submitted-email");
+
+
